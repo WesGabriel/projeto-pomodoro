7d0743aabb35506d25bef6751455ca57f761bfe1
Wesley Gabriel <wesgabriel79@gmail.com>
2024-08-29 09:20:47 -0300

    finalizando setup do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>Hello Word!</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="sucess" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
