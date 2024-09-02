e136ee20ffa1e4467302ed3d9ff02db10f588af9
Wesley Gabriel <wesgabriel79@gmail.com>
2024-09-02 14:03:39 -0300

    fix: finalizando pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,60 @@
 import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StarCountdownButton,
+  TaskInput,
+} from "./styles";
 
 export const Home = () => {
   return (
-    <div>
+    <HomeContainer>
       <form>
-        <div>
+        <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input type="" id="task" />
+          <TaskInput
+            type=""
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-sugestion"
+          />
+
+          <datalist id="task-sugestion">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
-        </div>
+        </FormContainer>
 
-        <div>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <Separator>:</Separator>
           <span>0</span>
           <span>0</span>
-        </div>
+        </CountdownContainer>
 
-        <button type="submit">
-          <Play />
+        <StarCountdownButton disabled type="submit">
+          <Play size={24} />
           Começar
-        </button>
+        </StarCountdownButton>
       </form>
-    </div>
+    </HomeContainer>
   );
 };
