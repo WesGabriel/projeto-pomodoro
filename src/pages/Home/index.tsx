4f6eb6e622f4c40d7bc79601473495102e4cdc06
Wesley Gabriel <wesgabriel79@gmail.com>
2024-09-06 13:41:54 -0300

    fix: corrigindo variaveis

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -105,10 +105,10 @@ export const Home = () => {
 
         <CountdownContainer>
           <span>{minutes[0]}</span>
-          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
           <span>{seconds[0]}</span>
-          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StarCountdownButton type="submit" disabled={isSubmitDisabled}>
